ea5b288540b983dc6c8fcd404ba4c7a0bfbe2a55
Jonathan Rico <jonathan@rico.live>
2024-07-07 22:11:27 +0200

    use "agent" name instead of default "lispy"
    Avoid disconnecting with #x8E "session taken over"

--- scratch.lisp
+++ scratch.lisp
@@ -41,20 +41,26 @@
 (with-fn-shadow ('publish #'fake-publish)
   (app-callback nil *rec4*))
 
-(mqtt:with-broker ("192.168.10.175" 1883 *broker*)
+(mqtt:with-broker ("192.168.10.175" 1883 *broker* :client-id-str "agent")
     (publish broker "z2m/light-manger/set/state" "ON")
     (sleep 1)
     (publish broker "z2m/light-manger/set/state" "OFF")
     )
 
-(mqtt:with-broker ("192.168.10.175" 1883 broker)
+(mqtt:with-broker ("192.168.10.175" 1883 broker :client-id-str "agent")
   (set-brightness broker "z2m/light-chambre" 250))
 
-(mqtt:with-broker ("192.168.10.175" 1883 broker)
+(mqtt:with-broker ("192.168.10.175" 1883 broker :client-id-str "agent")
     (publish broker "z2m/light-chambre/set/state" "OFF"))
 
-(mqtt:with-broker ("192.168.10.175" 1883 broker)
+(mqtt:with-broker ("192.168.10.175" 1883 broker :client-id-str "agent")
     (publish broker "z2m/light-chambre/set/state" "ON"))
+
+(mqtt:with-broker ("192.168.10.175" 1883 broker :client-id-str "agent")
+    (mqtt:publish broker "z2m/force-bureau/set/state" "on"))
+
+(mqtt:with-broker ("192.168.10.175" 1883 broker :client-id-str "agent")
+    (mqtt:publish broker "z2m/enable-bureau/set/state" "off"))
 
 ;; -------------------
 
