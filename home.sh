581b75e778eee8805638cabe99ab46b182541dac
Jonathan Rico <jonathan@rico.live>
2024-07-09 23:04:15 +0200

    add systemd unit

---
+++ home.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eu
+
+/usr/bin/sbcl \
+    --eval '(push "/home/john/hal9k/" ql:*local-project-directories*)' \
+    --eval '(ql:quickload "home")' \
+    --eval "(in-package :home)" \
+    --eval "(main)" \
+    --quit
