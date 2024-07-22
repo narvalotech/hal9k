4ec3051d60066b31e166c52caf625aa48f162696
Jonathan Rico <jonathan@rico.live>
2024-07-22 22:13:15 +0200

    wip: it sends
    code originally produced by phind and beaten into working by yours truly

--- web.lisp
+++ web.lisp
@@ -21,11 +21,47 @@
 ;; - office
 ;; - rest of house
 
+(defun render-slider (name)
+  (spinneret:with-html-string
+    (:li (:input :type :range
+                 :class "slider"
+                 :name name
+                 :min 0 :max 255 :step 1))))
+
 (defun render-toggle (name)
   (spinneret:with-html-string
-    (:form :action "" :method "POST"
-           (:input :type :hidden :name "action" :value "toggle")
-           (:li (:button :name name :value "toggle" name)))))
+    (:li (:button
+          :class "toggle-button"
+          :value name name))))
+
+(defparameter jsmain
+  (ps:ps
+    (defun send-put-request (url action value)
+      (let ((data (ps:create :action action :name value)))
+        (ps:chain (fetch url
+                         (ps:create :method "PUT"
+                                    :headers (ps:create "Content-Type" "application/json")
+                                    :body (ps:chain *json* (stringify data))))
+                  (then (lambda (response) (ps:@ response json)))
+                  (then (lambda (data) (ps:@ console (log data)))))))
+
+    ;; note: use "input" for events on value change
+    (defun setup-slider-event-listeners ()
+      (let ((sliders (ps:chain document (get-elements-by-class-name "slider"))))
+        (loop for slider across sliders
+              do (ps:chain slider (add-event-listener
+                                   "change"
+                                   (lambda () (send-put-request "/input" "slider" (ps:@ slider value))))))))
+
+    (defun setup-button-event-listeners ()
+      (let ((buttons (ps:chain document (get-elements-by-class-name "toggle-button"))))
+        (loop for button across buttons
+              do (ps:chain button (add-event-listener
+                                   "click"
+                                   (lambda () (send-put-request "/input" "toggle" (ps:@ button value))))))))
+
+    (setup-slider-event-listeners)
+    (setup-button-event-listeners)))
 
 (defun controls ()
   (spinneret:with-html-string
@@ -34,27 +70,24 @@
     (:body
      (:p "Lights")
      (:ul
-      (:li (:input :type :range :name "intensity" :id "intensity" :min 0 :max 255 :step 1))
       (:raw
+       (render-slider "chambre")
        (render-toggle "chambre")
-       (render-toggle "couloir"))))))
+       (render-toggle "couloir")))
+     (:script (:raw jsmain)))))
 
 (defun response (env)
   ;; (declare (ignore env))
   (format t "env: ~A~%" env)
   ;; (break)
-  (when (equal (getf env :request-method) :POST)
+  (when (equal (getf env :request-method) :PUT)
     (let* ((stream (getf env :raw-body)))
       (format t "=> payload: ~A~%"
               (read-line stream))))
 
   ;; Response:
   ;; [status code] [headers (plist)] [body (strings / vector / pathname)]
-  ;;
-  ;; '(200 (:content-type "text/plain") ("Hello dude"))
-  (list 200 '(:content-type "text/html") (list (controls)))
-
-  )
+  (list 200 '(:content-type "text/html") (list (controls))))
 
 (defvar *handler*
   (clack:clackup
