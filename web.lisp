aa86e808db65bd609c4be088bcdf8b92d6dbe995
Jonathan Rico <jonathan@rico.live>
2024-07-30 08:40:51 +0200

    [new UI] generate the HTML
    missing CSS & JS

--- web.lisp
+++ web.lisp
@@ -62,25 +62,90 @@
                        (toggle-light name)))
       (t (format t "Unknown type ~A~%" type)))))
 
-(defun render-slider (name class)
+(defparameter *svg-light-on*
+  "<svg class=\"light-on\" xmlns=\"http://www.w3.org/2000/svg\" width=\"100%\" height=\"100%\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"lucide lucide-lightbulb\">
+                            <path d=\"M15 14c.2-1 .7-1.7 1.5-2.5 1-.9 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5\"/>
+                            <path d=\"M9 18h6\"/>
+                            <path d=\"M10 22h4\"/>
+                        </svg>")
+
+(defparameter *svg-light-off*
+  "<svg class=\"light-off\" xmlns=\"http://www.w3.org/2000/svg\" width=\"100%\" height=\"100%\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"lucide lucide-lightbulb-off\">
+                            <path d=\"M16.8 11.2c.8-.9 1.2-2 1.2-3.2a6 6 0 0 0-9.3-5\"/>
+                            <path d=\"m2 2 20 20\"/>
+                            <path d=\"M6.3 6.3a4.67 4.67 0 0 0 1.2 5.2c.7.7 1.3 1.5 1.5 2.5\"/>
+                            <path d=\"M9 18h6\"/>
+                            <path d=\"M10 22h4\"/>
+                        </svg>")
+
+(defparameter *svg-heat-on*
+  "<svg class=\"heat-on\" xmlns=\"http://www.w3.org/2000/svg\" width=\"100%\" height=\"100%\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"lucide lucide-flame\">
+                            <path d=\"M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.38-.5-2-1-3-1.072-2.143-.224-4.054 2-6 .5 2.5 2 4.9 4 6.5 2 1.6 3 3.5 3 5.5a7 7 0 1 1-14 0c0-1.153.433-2.294 1-3a2.5 2.5 0 0 0 2.5 2.5z\"/>
+                        </svg>")
+
+(defparameter *svg-heat-off*
+  "<svg class=\"heat-off\" xmlns=\"http://www.w3.org/2000/svg\" width=\"100%\" height=\"100%\" viewBox=\"0 0 24 24\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"2\" stroke-linecap=\"round\" stroke-linejoin=\"round\" class=\"lucide lucide-snowflake\">
+                            <line x1=\"2\" x2=\"22\" y1=\"12\" y2=\"12\"/>
+                            <line x1=\"12\" x2=\"12\" y1=\"2\" y2=\"22\"/>
+                            <path d=\"m20 16-4-4 4-4\"/>
+                            <path d=\"m4 8 4 4-4 4\"/>
+                            <path d=\"m16 4-4 4-4-4\"/>
+                            <path d=\"m8 20 4-4 4 4\"/>
+                        </svg>")
+
+(apply #'concatenate 'string '("hello" "world"))
+
+(defun render-control-group (name &rest rendered-controls)
   (spinneret:with-html-string
-    (:li (:input :type :range
-                 :class class
-                 :name name
-                 :min 0 :max 255 :step 1))))
+    (:div :class "control-group"
+          (:span :class :label name)
+          (:raw
+           (apply #'concatenate 'string rendered-controls)))))
 
-(defun render-light-slider (name)
-  (render-slider name "light-slider"))
+(defun render-light (name)
+  (spinneret:with-html-string
+    (:div :class "control light" :data-name name
+          (:span :class "label light" name)
+          (:button :class "switch light" :data-state "on"
+                   (:raw *svg-light-on*)
+                   (:raw *svg-light-off*))
+          (:input :type :range
+                  :min 0
+                  :max 255
+                  :step 1
+                  :value 42
+                  :class "slider light"))))
 
-(defun render-toggle (name class)
+;; (format t "~A" (render-light "hello"))
+
+;; maybe a slider + value display would be better?
+(defun render-heater (name)
+  (spinneret:with-html-string
+    (:div :class "control heat" :data-name name
+          (:span :class "label heat" name)
+          (:button :class "switch heat" :data-state "on"
+                   (:raw *svg-heat-on*)
+                   (:raw *svg-heat-off*))
+          (:input :type :number
+                  :inputmode :numeric
+                  :enterkeyhint "done"
+                  :min 10
+                  :max 35))))
+
+;; (format t "~A" (render-heater "hello"))
+
+;; TODO:
+;; - render off by default
+;; - change button image when it's on or off
+(defun render-switch (name class)
   (spinneret:with-html-string
     (:li (:button
           :class class
           :name name
           :value name name))))
 
-(defun render-light-toggle (name)
-  (render-toggle name "light-toggle"))
+(defun render-light-switch (name)
+  (render-switch name "light-switch"))
 
 (defparameter jsmain
   (ps:ps
@@ -116,18 +181,28 @@
     (setup-slider-event-listeners)
     (setup-button-event-listeners)))
 
+;; TODO: remove
+(defparameter jsmain "")
+
 (defun controls ()
   (spinneret:with-html-string
     (:doctype)
-    (:head)
+    (:head
+     (:title "Hjem"))
     (:body
-     (:p "Lights")
-     (:ul
+     (:div :class "controls"
       (:raw
-       (render-light-slider "chambre")
-       (render-light-toggle "chambre")
-       (render-light-toggle "couloir")))
+       (render-control-group
+        "LIGHT"
+        (render-light "chambre")
+        (render-light "couloir"))
+       (render-control-group
+        "HEAT"
+        (render-heater "chambre")
+        (render-heater "rachel"))))
      (:script (:raw jsmain)))))
+
+(format t "~A" (controls))
 
 (defun response (env)
   (format t "env: ~A~%" env)
@@ -153,3 +228,7 @@
    'response))
 
 (clack:stop *handler*)
+
+(lass:compile-and-write
+ '(div
+   :background black))
