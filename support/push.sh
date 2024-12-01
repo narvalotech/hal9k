da4c80d2112296a711f572b182ced0f175ed1213
Jonathan Rico <jonathan@rico.live>
2024-12-01 20:07:10 +0100

    add proper systemd units for `web` and `push`
    and move all support files to support/

---
+++ support/push.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sbcl --non-interactive \
+    --load ~/hal9k/push.lisp
--- support/web.sh
+++ support/web.sh
@@ -3,6 +3,6 @@
 sbcl --non-interactive \
     --eval '(push "/home/john/hal9k/" ql:*local-project-directories*)' \
     --load ~/hal9k/web.lisp \
-    --eval '(format t "Hit enter to stop the server~%")' \
-    --eval '(read-line)' \
+    --eval '(defparameter *run-loop* t)' \
+    --eval '(loop while *run-loop* do (sleep 100))' \
     --eval '(clack:stop *handler*)'
